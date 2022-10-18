88f91bb2a0f95d919fa0403de099300b73da85d9
ManiGeek <maniweeb@gmail.com>
2022-10-18 18:11:40 +0330

    feat:CompletedAddTodo

---
+++ scripts/app.js
@@ -0,0 +1,77 @@
+let $=document
+let todoInputElm=$.querySelector('.todo-input')
+let addButtonElm=$.querySelector('.add-button')
+let todoListElm=$.querySelector('.todo-list')
+
+let todosArray = []
+
+
+function addTodo(event){
+    event.preventDefault();
+    let newTodoTitle = todoInputElm.value
+
+    let newTodoObj={
+        id: todosArray.length + 1,
+        title: newTodoTitle,
+        complete: false
+    }
+    todoInputElm.value = ''
+    todosArray.push(newTodoObj)
+    setLocalStorage(todosArray)
+    todosGenerator(todosArray)
+
+    todoInputElm.focus()
+
+}
+
+function todosGenerator(todosList){
+    todoListElm.innerHTML=''
+        todosList.forEach((todo) => {
+            console.log(todo);
+            todoListElm.insertAdjacentHTML('beforeend',`
+        <div class="todo">
+        <li class="todo__content">${todo.title}</li>
+        <button onclick='${completedTodo(event)}' class="completed">
+            <i class="fa fa-check" aria-hidden="true"></i>
+        </button>
+        <button onclick='${deleteTodo(event)}' class="delete">
+            <i class="fas fa-trash"></i>
+        </button>
+    </div>
+    `)
+        });
+        
+    }
+       
+
+
+function setLocalStorage(todoList){
+    console.log('this is a set');
+    localStorage.setItem('todos',JSON.stringify(todoList))
+}
+function getLocalStorage(){
+    console.log('this is a get');
+    let localStorageTodos=JSON.parse(localStorage.getItem('todos'))
+    if(localStorageTodos){
+        todosArray=localStorageTodos
+    }else{
+        todosArray=[]
+    }
+    todosGenerator(todosArray)
+}
+
+
+
+
+window.addEventListener('load', getLocalStorage)
+addButtonElm.addEventListener('click',addTodo)
+todoInputElm.addEventListener('keydown', function (event) {
+    
+    if (event.code === 'Enter') {
+        addNewTodo()
+    }
+})
+
+
+
+
